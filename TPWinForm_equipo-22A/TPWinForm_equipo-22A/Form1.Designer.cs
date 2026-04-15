85c811b70027b495823cbadddb19f02cce7ad1f9
AngelMed43 <tomas.oliveres@alumnos.frgp.utn.edu.ar>
2026-04-15 20:29:23 -0300

    Cambio en parte grafica, Articulos, Marcas y Categorias

--- TPWinForm_equipo-22A/TPWinForm_equipo-22A/Form1.Designer.cs
+++ TPWinForm_equipo-22A/TPWinForm_equipo-22A/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
@@ -38,19 +37,23 @@
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Articulos = new System.Windows.Forms.TabPage();
+            this.Marcas = new System.Windows.Forms.TabPage();
+            this.Categorias = new System.Windows.Forms.TabPage();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.Articulos.SuspendLayout();
+            this.Marcas.SuspendLayout();
+            this.Categorias.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(28, 110);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1631, 832);
-            this.dataGridView1.TabIndex = 1;
             // 
             // toolStrip1
             // 
@@ -63,7 +66,7 @@
             this.toolStripSeparator2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1698, 42);
+            this.toolStrip1.Size = new System.Drawing.Size(1722, 42);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -107,7 +110,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1262, 20);
+            this.label1.Location = new System.Drawing.Point(1268, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 25);
             this.label1.TabIndex = 3;
@@ -116,33 +119,121 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1372, 17);
+            this.textBox1.Location = new System.Drawing.Point(1376, 11);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(287, 31);
             this.textBox1.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Articulos);
+            this.tabControl1.Controls.Add(this.Marcas);
+            this.tabControl1.Controls.Add(this.Categorias);
+            this.tabControl1.Location = new System.Drawing.Point(0, 68);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1722, 886);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // Articulos
+            // 
+            this.Articulos.Controls.Add(this.dataGridView1);
+            this.Articulos.Location = new System.Drawing.Point(8, 39);
+            this.Articulos.Name = "Articulos";
+            this.Articulos.Padding = new System.Windows.Forms.Padding(3);
+            this.Articulos.Size = new System.Drawing.Size(1706, 839);
+            this.Articulos.TabIndex = 0;
+            this.Articulos.Text = "Articulos";
+            this.Articulos.UseVisualStyleBackColor = true;
+            // 
+            // Marcas
+            // 
+            this.Marcas.Controls.Add(this.dataGridView2);
+            this.Marcas.Location = new System.Drawing.Point(8, 39);
+            this.Marcas.Name = "Marcas";
+            this.Marcas.Padding = new System.Windows.Forms.Padding(3);
+            this.Marcas.Size = new System.Drawing.Size(1706, 839);
+            this.Marcas.TabIndex = 1;
+            this.Marcas.Text = "Marcas";
+            this.Marcas.UseVisualStyleBackColor = true;
+            // 
+            // Categorias
+            // 
+            this.Categorias.Controls.Add(this.dataGridView3);
+            this.Categorias.Location = new System.Drawing.Point(8, 39);
+            this.Categorias.Name = "Categorias";
+            this.Categorias.Size = new System.Drawing.Size(1706, 839);
+            this.Categorias.TabIndex = 2;
+            this.Categorias.Text = "Categorias";
+            this.Categorias.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 957);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1722, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1668, 794);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 27);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(1668, 794);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(16, 27);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 82;
+            this.dataGridView3.RowTemplate.Height = 33;
+            this.dataGridView3.Size = new System.Drawing.Size(1668, 794);
+            this.dataGridView3.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1698, 979);
+            this.ClientSize = new System.Drawing.Size(1722, 979);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Aplicacion de gestion";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.Articulos.ResumeLayout(false);
+            this.Marcas.ResumeLayout(false);
+            this.Categorias.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
@@ -151,6 +242,14 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Articulos;
+        private System.Windows.Forms.TabPage Marcas;
+        private System.Windows.Forms.TabPage Categorias;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
 
